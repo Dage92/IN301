50aef6902850160c3301457f041ec38287d7e492
Dage92 <david.goodman92@gmail.com>
2017-12-10 21:28:29 +0100

    Project Slider 1.0.1

--- Project Slider/lire_ecrire.c
+++ Project Slider/lire_ecrire.c
@@ -3,26 +3,37 @@
 #include "mes_types.h"
 
 
-SLIDER lire_fichier(char *argv[])
+SLIDER lire_fichier(char * argv[])
 {
-	FILE * S = fopen(argv[1],"r");
+	FILE * F = fopen(argv[1] , "r");
 	
-	char c;
+	printf("Nom du niveau = %s\n",argv[1]);
 	
-	while((c = fgetc(f)) != EOF)
+	if(F == NULL)
 	{
-		printf("%c", c);
+		printf("Erreur lors de l'ouverture de %s\n", argv[1]);
+	}
+	
+	SLIDER S;
+	
+	fscanf(F, "%d", S.L);
+	fscanf(F, "%d", S.H);
+	fscanf(F, "%d", S.x_slider);
+	fscanf(F, "%d", S.y_slider);
+	fscanf(F, "%d", S.x_exit);
+	fscanf(F, "%d", S.y_exit);
+	fscanf(F, "%d", S.N);
+	
+	while(fgetc(F) != EOF)
+	{
+		fscanf(F, "%d", S.xi);
+		fscanf(F, "%d", S.yi);
+		fscanf(F, "%d", S.di);
 	}
 	printf("\n");
 	
-	return 0;
-	printf("nom = %s\n",argv[1]);
-	SLIDER S;
-	S.L = 10;
-	S.H = 5;
-	
 	return S;
-	fclose(f);
+	fclose(F);
 }
 
 
